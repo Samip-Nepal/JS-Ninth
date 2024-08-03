25816bade6b91a8b0db48d27608539e54546d61b
Samip Nepal <samipnepal5@gmail.com>
2024-08-03 15:42:42 +1000

    Synchrouous and asynchronous process

---
+++ First.js
@@ -0,0 +1,12 @@
+//synchronous 
+console.log("One");
+console.log("two");
+console.log("three");
+//Asynchronous
+console.log("One");
+console.log("two");
+console.log("three");
+setTimeout(()=>{
+   hello= console.log("Hi"),9000
+});
+console.log("Four");
