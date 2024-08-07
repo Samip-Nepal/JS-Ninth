3e421fa0b26fd3baa36ebd2c1ee13004f7dd4158
Samip Nepal <samipnepal5@gmail.com>
2024-08-08 06:24:20 +1000

    Callback hell

--- First.js
+++ First.js
@@ -7,14 +7,36 @@ console.log("One");
 console.log("two");
 console.log("three");
 setTimeout(()=>{
-   hello= console.log("Hi"),9000
+   hello= console.log("Hi");9000
 });
 console.log("Four");*/
 //call backs
-function sum (a,b){
+/*function sum (a,b){
     console.log(a+b);
 }
 function calculator(a,b,callback) {
     callback(a,b);
 }
-calculator(3,4,sum);
+calculator(3,4,sum);*/
+
+//callback hell 
+function getdata (data,another_data){
+    setTimeout(()=>{
+        console.log("data=",data);
+        if (another_data){
+            another_data();
+        }
+    },2000);
+ 
+}
+getdata(1,()=>{
+    console.log("This is callback hell");
+    getdata(2,()=>{
+        console.log("This is callback hell");
+        getdata(3,()=>{
+            console.log("This is callback hell");
+            getdata(4);
+            
+        });
+    });
+});
